3fe69d521dc6a6783394b91be7e11873c78ea9c4
omaryasser <oyasser826@gmail.com>
2016-06-15 19:53:17 +0200

    Update 11572 - Unique Snowflakes.java

--- 11572 - Unique Snowflakes.java
+++ 11572 - Unique Snowflakes.java
@@ -32,26 +32,7 @@ public class Main {
         out.flush();
         out.close();
     }
-    static class Frosh implements  Comparable<Frosh>{
-        TreeSet<Integer> ts = new TreeSet<Integer>();
-        Frosh (int arr []) {for(int i = 0 ; i < 5 ; ++i) ts.add(arr[i]);}
-
-        @Override
-        public int compareTo(Frosh o) {
-            int first [] = new int[5] , second [] = new int[5];
-            int idx = 0; for(int i : ts) first[idx++] =i;
-            idx = 0 ;    for(int i : o.ts) second[idx++] =i;
-            for (int i = 0 ; i < 5 ;++i)
-                if (first[i] != second[i])
-                    return first[i] - second[i];
-            return 0 ;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return ts.equals(((Frosh)(obj)).ts);
-        }
-    }
+    
     static  class Scanner
     {
         StringTokenizer st;
